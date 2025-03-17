892d803629c8c9af7905352c3d9729a001043696
Ricardo Atakiama <ricardo_cssg@hotmail.com>
2025-03-17 01:08:32 -0300

    feat: nome do autor adiconado no texto e livros aparecem com mesmo espaço horizontal

--- script.js
+++ script.js
@@ -25,29 +25,59 @@ queryFetch(`
       }
     }
   }
-`).then(data => {
-  data.data.books.forEach(element => {
-    const li = document.createElement('li')
-    li.innerHTML = element.title
+`).then(async data => {
+  data.data.books.forEach(async element => {
+    const li = document.createElement('li');
+    li.innerHTML = `Autor: ${element.author.name}`
 
-    const img = document.createElement('img')
-    img.src = element.imageUrl
-    img.alt = element.title
-    img.width = 200
+    const p = document.createElement('p')
+    p.innerHTML = element.title;
+    li.appendChild(p);
 
-    const handleClick = () => {
-      window.location.href = element.downloadUrl
+    const img = document.createElement('img');
+    img.src = element.imageUrl;
+    img.alt = element.title;
+    img.width = 200;
+
+    li.appendChild(img);
+
+    catalogList.append(li);
+
+    const fileUrl = element.downloadUrl;
+
+    if (fileUrl) {
+      img.addEventListener('click', async () => {
+        try {
+          await downloadPdf(fileUrl, element.title);
+        } catch (error) {
+          console.error('Erro ao baixar arquivo:', error);
+        }
+      });
     }
-
-    img.addEventListener('click', handleClick)
-
-    li.addEventListener('DOMNodeRemoved', () => {
-      img.removeEventListener('click', handleClick)
-    })
-
-    li.appendChild(img)
-
-    catalogList.append(li)
   });
 })
 
+async function downloadPdf(pdfUrl, fileName) {
+  fetch(`http://localhost:3000/proxy/download-pdf?url=${pdfUrl}`, {
+    headers: {
+      'Content-Type': 'application/pdf'
+    }
+  })
+    .then(response => response.blob())
+    .then(blob => {
+      const url = window.URL.createObjectURL(blob);
+
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = fileName;
+      document.body.appendChild(link);
+
+      link.click();
+
+      window.URL.revokeObjectURL(url);
+      document.body.removeChild(link);
+    })
+    .catch(error => {
+      console.error("Download do PDF falhou: ", error);
+    });
+}
